f82dd1c4bd87b1014c97ecb388126e99671b4e51
Игорь <flagmr040@gmail.com>
2024-03-26 21:57:58 +0200

    bugs fixed

--- src/Program.java
+++ src/Program.java
@@ -1,6 +1,6 @@
 public class Program {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         User user = new User("Alice", "qwerty");
-        user.displayAccount();
+        user.acount.displayAcount();
     }
 }
--- src/User.java
+++ src/User.java
@@ -1,23 +1,18 @@
 class User {
     private String login;
-    private Acount acount;
+    public Acount acount;
 
     public User(String login, String password) {
         this.login = login;
         this.acount = new Acount(password);
     }
-    private class Acount{
-        private String password;
-
+    class Acount {
+        private String password; // Пароль
         public Acount(String password) {
             this.password = password;
-    }
-        public void displayAccount() {
-            System.out.println("Вход в аккаунт успешен! "+ login + "\nПароль " + password);
         }
-    }
-    // Метод для виклику методу внутрішнього класу Account
-    public void displayAccount() {
-        acount.displayAccount();
+        public void displayAcount() {
+            System.out.println("Вход в аккаунт успешен! Авторизоваться: " + login + "\nПароль: " + password);
+        }
     }
 }
